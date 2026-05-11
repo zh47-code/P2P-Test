3f16d339232921d764497b7309619b579794a246
Zul <asyari.zul@gmail.com>
2026-05-11 14:17:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,29 @@
-const CACHE_NAME = "p2p-chat-v1";
+const CACHE_NAME = 'p2p-chat-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install Service Worker
-self.addEventListener("install", event => {
+// Install Service Worker dan simpan cache
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Fetch Data (Bisa dibuka saat offline)
-self.addEventListener("fetch", event => {
+// Ambil dari cache jika offline
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        // Return file dari cache jika ada, jika tidak ambil dari internet
+        return response || fetch(event.request);
+      })
   );
 });
